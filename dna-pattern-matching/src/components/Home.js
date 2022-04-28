54f3b75ae9fbc896aecae4151eb7cc7519201c01
Cryptofal <88304292+FaLzNaufal@users.noreply.github.com>
2022-04-28 17:11:12 +0700

    Update Home.js

--- dna-pattern-matching/src/components/Home.js
+++ dna-pattern-matching/src/components/Home.js
@@ -5,9 +5,9 @@ const Home = () => {
     <div className="text-center">
       <h1 className="text-2xl font-bold py-4">DNA PATTERN MATCHING</h1>
       <h2 className="text-1xl font-bold py-4">Developed by: </h2>
-      <ul>Lorem ipsum dolor sit amet.</ul>
-      <ul>Lorem ipsum dolor sit amet.</ul>
-      <ul>Lorem ipsum dolor sit amet.</ul>
+      <ul>Muhammad Naufal Satriandana 13520068</ul>
+      <ul>Vincent Ho 13520093</ul>
+      <ul>Farrel Ahmad 13520110</ul>
     </div>
   );
 };
